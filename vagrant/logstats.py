1c297fed059b64862b19513aabd88bd0f39f40bf
benlove <benuklove@gmail.com>
2018-01-16 11:39:08 -0600

    Cleaned logstats for pep8

--- vagrant/logstats.py
+++ vagrant/logstats.py
@@ -1,19 +1,26 @@
 #!/home/benlove/projects/FSND-Virtual-Machine/venv/bin/python3
 
-# Database code for log analysis project
+"""Database code for log analysis project."""
 
 import psycopg2
 
 
 def main():
+    """One function to rule them all."""
     output_result()
 
 
-# Most popular three articles of all time?
 def popular_articles():
+    """Most popular three articles of all time.
+    Connect, query, return result, and close connection.
+    Format result for easy reading.
+    """
     pg = psycopg2.connect(dbname="news")
     c = pg.cursor()
-    query = "select title, count(*) as views from articles as t1 join log as t2 on position(t1.slug in t2.path)<>0 where t2.status = '200 OK' group by title order by views desc limit 3;"
+    query = "select title, count(*) as views from articles\
+                as t1 join log as t2 on position(t1.slug in t2.path)<>0\
+                 where t2.status = '200 OK'\
+                  group by title order by views desc limit 3;"
     c.execute(query)
     result = c.fetchall()
     articles = ""
@@ -23,12 +30,20 @@ def popular_articles():
     pg.close()
     return articles
 
-# Most popular authors of all time, sorted by article views?
+
 def popular_authors():
+    """Most popular authors of all time, sorted by article views.
+    Connect, query, return result, and close connection.
+    Format result for easy reading.
+    """
     pg = psycopg2.connect(dbname="news")
     c = pg.cursor()
-    base_query = "(select slug, name from articles join authors on articles.author = authors.id)"
-    query = "select name, count(*) as views from {} as t1 join log as t2 on position(t1.slug in t2.path)<>0 where t2.status = '200 OK' group by name order by views desc;".format(base_query)
+    base_query = "(select slug, name from articles join authors\
+                     on articles.author = authors.id)"
+    query = "select name, count(*) as views\
+             from {} as t1 join log as t2 on position(t1.slug in t2.path)<>0\
+              where t2.status = '200 OK' group by name order by views desc;"\
+              .format(base_query)
     c.execute(query)
     result = c.fetchall()
     authors = ""
@@ -38,13 +53,28 @@ def popular_authors():
     pg.close()
     return authors
 
-# On which days did more than 1% of requests lead to errors?
+
 def high_errors():
+    """On which days did more than 1% of requests lead to errors?
+    Connect, query, return result, and close connection.
+    In one query, two tables are made:
+    one with columns of (day, successes),
+    the other with columns of (day, errors).
+    They are joined into a table with (day, percent).
+    Format result for easy reading.
+    """
     pg = psycopg2.connect(dbname="news")
     c = pg.cursor()
-    table_1 = "(select date_trunc('day', log.time) \"day\", count(*) as successes from log where status = '200 OK' group by 1 order by 1)"
-    table_2 = "(select date_trunc('day', log.time) \"day\", count(*) as errors from log where status != '200 OK' group by 1 order by 1)"
-    query = "select ok.day, (1.0*errors/successes) as percent from {} as ok join {} as err on ok.day = err.day where (1.0*errors/successes)>0.01;".format(table_1, table_2)
+    table_1 = "(select date_trunc('day', log.time) \"day\",\
+                 count(*) as successes from log where status = '200 OK'\
+                  group by 1 order by 1)"
+    table_2 = "(select date_trunc('day', log.time) \"day\",\
+                 count(*) as errors from log where status != '200 OK'\
+                  group by 1 order by 1)"
+    query = "select ok.day, (1.0*errors/successes) as percent from {} as ok join {} as err\
+             on ok.day = err.day \
+             where (1.0*errors/successes)>0.01;"\
+             .format(table_1, table_2)
     c.execute(query)
     result = c.fetchall()
     errors = ""
@@ -57,15 +87,27 @@ def high_errors():
     pg.close()
     return errors
 
-# Log results to file
+
 def output_result():
+    """Log results to file"""
     errors = high_errors()
     authors = popular_authors()
     articles = popular_articles()
     with open("output.txt", "a") as text_file:
-        text_file.write("Top three most popular articles\n--------------------------------------------------\n{}\n\n".format(articles))
-        text_file.write("Most popular authors of all time, sorted by article views\n---------------------------------------------------------\n{}\n\n".format(authors))
-        text_file.write("Days with errors greater than 1%\n--------------------------------\n{}\n\n".format(errors))
+
+        text_file.write("Top three most popular articles\n"
+                        "--------------------------------------------------"
+                        "\n{}\n\n"\
+                        .format(articles))
+
+        text_file.write("Most popular authors of all time,"
+                        " sorted by article views\n--------------------------"
+                        "-------------------------------\n{}\n\n"
+                        .format(authors))
+
+        text_file.write("Days with errors greater than 1%\n"
+                        "--------------------------------\n{}\n\n"
+                        .format(errors))
 
 
 if __name__ == '__main__':
