bc2fce6d01dffccc8875ba825abccfcc94244e36
benlove <benuklove@gmail.com>
2018-01-15 20:53:58 -0600

    Initial framework for python to query db

---
+++ vagrant/logstats.py
@@ -0,0 +1,36 @@
+#!/home/benlove/projects/FSND-Virtual-Machine/venv/bin/ python3
+
+# Database code for log analysis project
+
+import psycopg2
+
+
+def main():
+    popular_articles()
+    popular_authors
+    high_errors()
+    output_result()
+
+
+# What are the most popular three articles of all time?
+def popular_articles():
+    pass
+
+# Who are the most popular article authors of all time?
+def popular_authors():
+    pass
+
+# On which days did more than 1% of requests lead to errors?
+def high_errors():
+    pass
+
+# Log success/error counts
+def output_result():
+    pass
+# with open("output.txt", "a") as text_file:
+#     text_file.write("Successes: {} ".format(successes))
+#     text_file.write("Errors: {}\n".format(errors))
+
+
+if __name__ == '__main__':
+    main()
