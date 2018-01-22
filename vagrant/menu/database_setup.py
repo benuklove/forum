c8b88b6df64c954afac373a1a08968f83ec81a49
benlove <benuklove@gmail.com>
2018-01-22 11:18:55 -0600

    Create menu directory, db setup file.

---
+++ vagrant/menu/database_setup.py
@@ -0,0 +1,53 @@
+# sys module for functions and variables to manipulate different parts
+# of the python runtime environment
+import sys
+
+# Import handy classes for mapper code
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+# Used in configuration and class code
+from sqlalchemy.ext.declarative import declarative_base
+
+# When we write up our mapper, used to create foreign key relationships
+from sqlalchemy.orm import relationship
+
+# Used in configuration code at end of file
+from sqlalchemy import create_engine
+
+# Lets SQLAlchemy know that our classes are special SQL classes
+Base = declarative_base()
+
+class Restaurant(Base):
+    __table_name__ = 'restaurant'
+    name = Column(
+        String(80),
+        nullable = False,
+    )
+    id = Column(
+        Integer,
+        primary_key = True
+    )
+
+class MenuItem(Base):
+    __table_name__ = 'menu_item'
+        name = Column(
+        String(80),
+        nullable = False,
+    )
+    id = Column(
+        Integer,
+        primary_key = True
+    )
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(80))
+    restaurant_id = Column(
+        Integer,
+        ForeignKey('restaurant.id')
+    )
+    restaurant = relationship(Restaurant)
+
+## Insert at End of File ##
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
